447d7fb8bab9161038633f246cdfd963a572eb9b
britnin <baekseongsa@gmail.com>
2021-05-01 20:50:03 +0900

    feat: 자동차 이름 길이 유효성 검사 추가

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -5,7 +5,15 @@ public class RacingCar {
 	private final String name;
 
 	public RacingCar(String name) {
+		validateName(name);
+
 		this.name = name;
+	}
+
+	private void validateName(String name) {
+		if (name == null || name.trim().isEmpty() || name.length() > 5) {
+			throw new IllegalArgumentException("자동자 이름이 존재하지 않거나, 길이가 5 이하여야 합니다.");
+		}
 	}
 
 	public String getName() {
