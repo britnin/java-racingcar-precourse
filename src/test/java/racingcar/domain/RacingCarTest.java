b91f963aac1fd3d0cc6382197bba41f597b4bd02
britnin <baekseongsa@gmail.com>
2021-05-01 20:50:16 +0900

    test: 자동차 이름 길이 유효성 검사 테스트 코드 작성

--- src/test/java/racingcar/domain/RacingCarTest.java
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RacingCarTest {
 
@@ -12,6 +15,15 @@ class RacingCarTest {
 	void successToCreateRacingCarWithName() {
 		RacingCar car = new RacingCar("테스트");
 		assertThat(car.getName()).isEqualTo("테스트");
+	}
+
+	@DisplayName("이름이 존재하지 않거나 길이가 5 초과하면 자동차를 생성할 수 없다.")
+	@ParameterizedTest
+	@NullAndEmptySource
+	@ValueSource(strings = { "123456" })
+	void failToCreateRacingCarIfNameIsNullOrEmptyOrLength5Exceeds(String name) {
+		assertThatThrownBy(() -> new RacingCar(name))
+			.isInstanceOf(IllegalArgumentException.class);
 	}
 
 }
