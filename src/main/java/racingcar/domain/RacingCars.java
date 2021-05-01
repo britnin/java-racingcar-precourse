0daa37358cf821fb29cfe03da25f4aa726bb5eff
britnin <baekseongsa@gmail.com>
2021-05-02 00:20:25 +0900

    feat: 경주할 자동차 일급 컬렉션 구현

---
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -0,0 +1,31 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class RacingCars {
+
+	private final List<RacingCar> racingCars;
+
+	public RacingCars(List<RacingCar> racingCars) {
+		validate(racingCars);
+
+		this.racingCars = racingCars;
+	}
+
+	private void validate(List<RacingCar> racingCars) {
+		if (racingCars == null || racingCars.size() == 0) {
+			throw new IllegalArgumentException("경주할 자동자들이 존재하지 않습니다.");
+		}
+
+		for (RacingCar racingCar : racingCars) {
+			validateRacingCar(racingCar);
+		}
+	}
+
+	private void validateRacingCar(RacingCar racingCar) {
+		if (racingCar == null) {
+			throw new IllegalArgumentException("경주할 자동차가 존재하지 않습니다.");
+		}
+	}
+
+}
