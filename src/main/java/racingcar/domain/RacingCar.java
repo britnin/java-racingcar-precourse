752972a12a6010e1123faccf428cdda46ca66d0f
britnin <baekseongsa@gmail.com>
2021-05-01 20:48:23 +0900

    feat: 이름을 가진 자동차 클래스 구현

---
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+public class RacingCar {
+
+	private final String name;
+
+	public RacingCar(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
